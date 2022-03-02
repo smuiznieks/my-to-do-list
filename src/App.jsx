c0a9f0bbac9adfc368f79793bd677d1b9cdbfb32
Selga P. Muiznieks <selga.muiznieks@gmail.com>
2022-03-01 20:00:33 -0500

    office hours week 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,66 @@
 import { useState } from 'react';
+import uuid from 'react-uuid';
 import './App.css';
 import Task from './components/Task';
 
 const initialValues = [
   {
-    id: 1,
+    id: uuid(),
     description: "Learn React",
+    isComplete: false,
   },
   {
-    id: 2,
+    id: uuid(),
     description: "Make dinner",
+    isComplete: false,
   },
   {
-    id: 3,
+    id: uuid(),
     description: "Oil change",
+    isComplete: false,
   },
   {
-    id: 4,
+    id: uuid(),
     description: "Laundry",
+    isComplete: false,
   },
 ];
 
 function App() {
   const [tasks, setTasks] = useState(initialValues);
+  const [newTask, setNewTask] = useState('');
 
   const handleSave = (e) => {
-    setTasks([...tasks, {description: 'work in progress!'}])
+    e.preventDefault();
+    setTasks([...tasks, {id: uuid(), description: newTask}]);
+    setNewTask('');
+  }
+
+  const handleInput = (e) => {
+    setNewTask(e.target.value);
+  }
+
+  const handleDelete = (deleteId) => {
+    let filtered = tasks.filter(x => x.id !== deleteId);
+    setTasks(filtered);
   }
 
   return (
     <div className="container">
       <h1>To Do List</h1>
       <div className='tasks'>
-        {tasks.map((task, index) => (
-          <Task task={task} key={index} />
-        ))}
+        <ul className="list-group">
+          {tasks.map((task, index) => (
+            <Task task={task} key={index} handleDelete={handleDelete}/>
+          ))}
+        </ul>
       </div>
-      <input type="text" className="form-control" placeholder="Don't forget!" aria-label="New Task" aria-describedby="basic-addon1"></input>
-      <button className='btn btn-primary' aria-label='Submit'>+</button>
+      <form>
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" placeholder="Don't forget!" aria-label="New Task" onChange={handleInput} value={newTask}/>
+          <button className="btn btn-outline-primary" type="button" id="save" onClick={handleSave}>+</button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,15 @@
-const Task = ({task}) => {
+const Task = ({task, handleDelete}) => {
   return (
-    <p>{task.description}</p>
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      <span>
+        <input className="form-check-input me-1" type="checkbox" value={task.isComplete} aria-label="..."/>
+        {task.description}
+      </span>
+      <div className="btn-group" role="group" aria-label="Basic example">
+        <button type="button" className="btn btn-primary">Edit</button>
+        <button type="button" className="btn btn-danger" onClick={() => handleDelete(task.id)}>Delete</button>
+      </div>
+    </li>
   )
 }
 
