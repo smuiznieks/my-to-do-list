a8fbc23fafe6d985e6f6bd38ee058093ebeb3e10
Selga P. Muiznieks <selga.muiznieks@gmail.com>
2022-04-05 19:17:36 -0400

    upgrade to React 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,13 +10,27 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
-ReactDOM.render(
+
+// REACT 17:
+
+// ReactDOM.render(
+  // <React.StrictMode>
+  //   <Router>
+  //     <App />
+  //   </Router>
+  // </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// REACT 18: 
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
